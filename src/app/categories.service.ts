eedb332262d82b8b157a71ab4ac6478e8f2a2627
Jenna El-Amin <jenna@mutuallyhuman.com>
2019-03-22 16:57:00 -0400

    Spike to investigate why some categories aren't populated

--- src/app/categories.service.ts
+++ src/app/categories.service.ts
@@ -1,38 +1,42 @@
-import { Injectable } from '@angular/core';
-import {  } from './categories.component';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
-import { tap } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import {} from "./categories.component";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import { of } from "rxjs/observable/of";
+import { tap } from "rxjs/operators";
 interface KitsuCategoryResponse {
   data: {
     id: any;
     attributes: {
+      nsfw: boolean;
+      totalMediaCount: number;
       title: string;
-    }
+    };
   }[];
 }
 
 export interface Category {
   id: any;
+  nsfw: boolean;
+  totalMediaCount: number;
   title: any;
 }
 
 @Injectable()
 export class CategoriesService {
   cache: KitsuCategoryResponse | null;
-  categoriesURL = "https://kitsu.io/api/edge/categories?fields[categories]=title&page%5Blimit%5D=1000";
+  categoriesURL = "https://kitsu.io/api/edge/categories?fields[categories]=nsfw,totalMediaCount,title&page%5Blimit%5D=1000";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   fetchFromKitsu(url: string): Observable<KitsuCategoryResponse> {
     const categoriesData = this.cache;
     if (categoriesData) {
       return of(categoriesData);
     } else {
-      return this.http.get<KitsuCategoryResponse>(this.categoriesURL).pipe(
-        tap(response => this.cache = response)
-      );
+      return this.http
+        .get<KitsuCategoryResponse>(this.categoriesURL)
+        .pipe(tap(response => (this.cache = response)));
     }
   }
 
@@ -40,13 +44,14 @@ export class CategoriesService {
     const categoriesRequest$ = this.fetchFromKitsu(this.categoriesURL);
 
     return categoriesRequest$.map(someResult => {
-     return someResult.data.map(category => {
+      return someResult.data.map(category => {
         return {
           id: category.id,
+          nsfw: category.attributes.nsfw,
+          totalMediaCount: category.attributes.totalMediaCount,
           title: category.attributes.title
         };
       });
     });
   }
-
 }
